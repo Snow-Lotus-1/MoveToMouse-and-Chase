4e7b50195e263788a647e8e7254ec0e7918eef71
Snow-Lotus-1 <70484240+Snow-Lotus-1@users.noreply.github.com>
2021-02-21 01:35:12 -0500

    Add files via upload

---
+++ MouseMovementTest/MouseMovementTest/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MouseMovementTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrMove = new System.Windows.Forms.Timer(this.components);
+            this.lblCursorX = new System.Windows.Forms.Label();
+            this.lblCursorY = new System.Windows.Forms.Label();
+            this.tmrEnemyMove = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrMove
+            // 
+            this.tmrMove.Interval = 1;
+            this.tmrMove.Tick += new System.EventHandler(this.tmrMove_Tick);
+            // 
+            // lblCursorX
+            // 
+            this.lblCursorX.AutoSize = true;
+            this.lblCursorX.Location = new System.Drawing.Point(316, 141);
+            this.lblCursorX.Name = "lblCursorX";
+            this.lblCursorX.Size = new System.Drawing.Size(35, 13);
+            this.lblCursorX.TabIndex = 0;
+            this.lblCursorX.Text = "label1";
+            // 
+            // lblCursorY
+            // 
+            this.lblCursorY.AutoSize = true;
+            this.lblCursorY.Location = new System.Drawing.Point(316, 154);
+            this.lblCursorY.Name = "lblCursorY";
+            this.lblCursorY.Size = new System.Drawing.Size(35, 13);
+            this.lblCursorY.TabIndex = 1;
+            this.lblCursorY.Text = "label1";
+            // 
+            // tmrEnemyMove
+            // 
+            this.tmrEnemyMove.Enabled = true;
+            this.tmrEnemyMove.Interval = 1;
+            this.tmrEnemyMove.Tick += new System.EventHandler(this.tmrEnemyMove_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1331, 837);
+            this.Controls.Add(this.lblCursorY);
+            this.Controls.Add(this.lblCursorX);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrMove;
+        private System.Windows.Forms.Label lblCursorX;
+        private System.Windows.Forms.Label lblCursorY;
+        private System.Windows.Forms.Timer tmrEnemyMove;
+    }
+}
+
---
+++ MouseMovementTest/MouseMovementTest/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMovementTest
+{
+    public partial class Form1 : Form
+    {
+        //Basic 
+        Rectangle playerBox, enemyBox;
+        Point cursorLocation;
+        double cursorX, cursorY;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //create playerBox
+            playerBox = new Rectangle(100, 100, 100, 100);
+
+            //create enemyBox
+            enemyBox = new Rectangle(0, 0, 100, 100);
+        }
+
+        private void tmrMove_Tick(object sender, EventArgs e)
+        {            
+            //Checks if the playerBox is at the cursor location
+            if ((playerBox.X + (playerBox.Width/2)) == (int)cursorX && (playerBox.Y + (playerBox.Height / 2)) == (int)cursorY)
+            {
+                tmrMove.Enabled = false;//if the playerBox is at the cursor location, disable timer to stop movement 
+            }
+
+            //if player is left of cursor more 10 pixals right
+            if ((playerBox.X + (playerBox.Width / 2)) < cursorX)
+            {
+                playerBox.X = playerBox.X + 10;
+            }
+            //if player is right of cursor more 10 pixals left
+            else if ((playerBox.X + (playerBox.Width / 2)) > cursorX)
+            {
+                playerBox.X = playerBox.X - 10;
+            }
+
+            //if player is above of cursor more 10 pixals down
+            if ((playerBox.Y + (playerBox.Height / 2)) < cursorY)
+            {
+                playerBox.Y = playerBox.Y + 10;
+            }
+            //if player is below of cursor more 10 pixals up
+            else if ((playerBox.Y + (playerBox.Height / 2)) > cursorY)
+            {
+                playerBox.Y = playerBox.Y - 10;
+            }
+
+            //refreshes rectangle location
+            Refresh();
+        }
+
+        //enemy movement system 
+        private void tmrEnemyMove_Tick(object sender, EventArgs e)
+        {
+            //if enemy is on the left side of player...
+            if (enemyBox.X < playerBox.X)
+            {
+                enemyBox.X = enemyBox.X + 5;//move enemy 5 pixals right
+            }
+            //if enemy is on the right side of player...
+            else if (enemyBox.X > playerBox.X)
+            {
+                enemyBox.X = enemyBox.X - 5;//move enemy 5 pixals left
+            }
+
+            //if enemy is on top of player...
+            if (enemyBox.Y < playerBox.Y)
+            {
+                enemyBox.Y = enemyBox.Y + 5;//move enemy 5 pixals down
+            }
+            //if enemy is below the of player...
+            else if (enemyBox.Y > playerBox.Y)
+            {
+                enemyBox.Y = enemyBox.Y - 5;//move enemy 5 pixals up
+            }
+
+            //refreshes rectangle location
+            Refresh();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //have cursorLocation be the location of cursors X and Y
+            cursorLocation = e.Location;
+
+            //split cursorLocation into cursorX and cursorY, in other words the cursor's X and Y location
+            cursorX = cursorLocation.X;
+            cursorY = cursorLocation.Y;            
+
+            //*We want the cursor values to be rounded to the nearest tenth*
+
+            //divide cursor X and Y by 100 to create decimals for rounding
+            cursorX = cursorX / 100;
+            cursorY = cursorY / 100;
+
+            //rounds both cursor X and Y to first decimal place then muliply by 100
+            cursorX = Math.Round(cursorX, 1) * 100;
+            cursorY = Math.Round(cursorY, 1) * 100;
+
+            //create temp cursor value to refrence orginal ones to later
+            double tempCursorX = cursorX;
+            double tempCursorY = cursorY;
+
+            //ceiling the cursor value to make sure if there were any problems with rounding (EX: 9.999999) it is fixed
+            //however, may lead to other problems (EX: 10.9)
+            cursorX = Math.Ceiling(cursorX);
+            cursorY = Math.Ceiling(cursorY);
+
+          
+
+
+            //have cursor values display to labels to check values during testing
+            lblCursorX.Text = cursorX.ToString();
+            lblCursorY.Text = cursorY.ToString();
+
+            //enable timer to let the player move
+            tmrMove.Enabled = true;
+        }        
+       
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            //have playerBox be green
+            e.Graphics.FillRectangle(Brushes.Green, playerBox);
+
+            //have enemyBox be red
+            e.Graphics.FillRectangle(Brushes.Red, enemyBox);
+        }
+    }
+}
